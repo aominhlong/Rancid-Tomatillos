b546d28c31b92cd26740a71e361c0ecfb5acc937
Nicholas Ao <aominhlong@yahoo.com>
2022-05-11 19:44:01 -0600

    Remove unnecessary files and add App skeleton

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import '../styles/App.css';
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor() {
+      super()
+      this.state = {}
+  }
+  render() {
+      return(
+        <main>
+
+        </main>
+      )
+  }
+}
+
+export default App;
