c3eeea26f14d180b6a35b24cf4183624e49d2506
Kal <kal.aalrajhi@gmail.com>
2022-05-12 13:22:52 -0600

    Populate sttate with movies array

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import '../styles/App.css';
 import React, {Component} from 'react';
 import MovieDetailsContainer from './MovieDetailsContainer';
+import movieData from '../movieData';
+
 
 class App extends Component {
   constructor() {
       super()
-      this.state = {}
+      this.state = {
+        movies: movieData.movies
+      }
   }
   render() {
       return(
