fed48e0610196c669255c9d641e0ed2c7f75b1f1
Kal <kal.aalrajhi@gmail.com>
2022-05-13 17:42:23 -0600

    Add error handling for 400+ HTTP errors

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,32 @@ class App extends Component {
       super()
       this.state = {
         movies: {},
-        currentMovie: {}
+        currentMovie: {},
+        error: ''
       }
   }
 
   componentDidMount = () => {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    // fetch('https://rancid-tomatillos.herokuapp.com/api/v2/mo')
+    fetch('https://httpstat.us/500')
+    .then(response => {
+      // console.log(response);
+      // if(response.status >= 400) {
+      //   console.log('HTTP request unsuccessful 400');
+      // }
+      if(!response.ok) {
+        console.log('HTTP request unsuccessful');
+        console.log('Error: ', response.status);
+        throw new Error(`status ${response.status} at URL: ${response.url}`)
+      } else {
+        console.log('HTTP request successful');
+      }
+      
+      return response;
+    })
     .then(response => response.json())
     .then(movies => this.setState({ movies: movies }))
-    .catch(err => console.log('Error: ', err));
+    .catch(err => console.log(err));
   }
 
   loadMovieDetails = (id) => {
