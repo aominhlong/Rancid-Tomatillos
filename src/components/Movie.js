cc01629ac6fc5eef46735cd684614ef12d69b5b0
Nicholas Ao <aominhlong@yahoo.com>
2022-05-14 14:16:43 -0600

    Add home and categories for nav bar and fix spacing for MovieContainer

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,8 +6,8 @@ const Movie = ({ id, poster_path, title, average_rating, loadMovieDetails }) => 
     return(
         <div onClick={ () => loadMovieDetails(id)} className='movie-card' id={id}>
             <img className='movie-poster-img' src={poster_path} alt={title + " movie poster"} />
-            <h3>{title}</h3>
-            <p>Rating: {Math.round(average_rating)} / 10</p>
+            {/* <h3>{title}</h3>
+            <p>Rating: {Math.round(average_rating)} / 10</p> */}
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,23 @@ import '../styles/NavBar.css'
 const NavBar = ({goHome}) => {
     return(
         <div className='nav-bar-container'>
-            <h1>Rancid Tamatillos</h1>
-            <button onClick={() => goHome()}>Home</button>
+            <div className='title-img'>
+                <img src="https://images.vexels.com/media/users/3/132481/isolated/preview/9fedd03df8eb7e962d492dac4a7cc081-icono-de-c--rculo-de-tomate-by-vexels.png" alt='tomatillos icon image'></img>
+                <h1>Rancid Tamatillos</h1>
+            </div>
+            <form className='button-input-container'>
+                <button className='home' onClick={() => goHome()}>Home</button>
+                <div className='dropdown'>
+                    <button className='categories'>Categories</button>
+                    <div className='dropdown-content'>
+                        <a href='#'>Action</a>
+                        <a href='#'>Adventure</a>
+                        <a href='#'>Horror</a>
+                        <a href='#'>Comedy</a>
+                    </div>
+                </div>
+                <input type="text" placeholder="find a movie"></input>
+            </form>
         </div>
     )
 }
