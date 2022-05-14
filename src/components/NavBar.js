7f2248efe279c75d56eb3fe9ff1b9a1582f85d13
Nicholas Ao <aominhlong@yahoo.com>
2022-05-14 12:17:33 -0600

    Add Movie styling for landing page and basic colors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import '../styles/NavBar.css'
 
 const NavBar = ({goHome}) => {
     return(
-        <div>
+        <div className='nav-bar-container'>
             <h1>Rancid Tamatillos</h1>
             <button onClick={() => goHome()}>Home</button>
         </div>
