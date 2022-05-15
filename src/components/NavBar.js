0c3c524917269aaf1d276d828d13e6d45463b929
Kal <kal.aalrajhi@gmail.com>
2022-05-15 16:51:41 -0600

    Refactor navbar design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,49 @@
 import React from 'react';
 import '../styles/NavBar.css'
 
-const NavBar = ({goHome, handleChange}) => {
+const NavBar = ({ goHome, handleChange }) => {
     return(
-        <div className='nav-bar-container'>
-            <div className='title-img'>
-                <img src="https://images.vexels.com/media/users/3/132481/isolated/preview/9fedd03df8eb7e962d492dac4a7cc081-icono-de-c--rculo-de-tomate-by-vexels.png" alt='tomatillos icon image'></img>
-                <h1>Rancid Tamatillos</h1>
+        <nav className="main-navbar" aria-label="Main">
+            <div className="container flex" aria-description="Navigation container with site name and navigation options">
+                <h1 className="site-title">Rotten Tamatillos</h1>
+                <nav>
+                    <ul>
+                        <li aria-setsize="3" aria-posinset="1"><button className="main-link" id="navHomeBtn">Home</button></li>
+                        <li aria-setsize="3" aria-posinset="2"><button className="main-link" id="navDashboardBtn">Dashboard</button></li>
+                        <li aria-setsize="3" aria-posinset="3"><button className="main-link" id="navBookBtn">Book</button></li>
+                    </ul>
+                </nav>
             </div>
-            <form className='button-input-container'>
-                <button className='home' onClick={() => goHome()}>Home</button>
-                <div className='dropdown'>
-                    <button className='categories'>Categories</button>
-                    <div className='dropdown-content'>
-                        <a href='#'>Action</a>
-                        <a href='#'>Adventure</a>
-                        <a href='#'>Horror</a>
-                        <a href='#'>Comedy</a>
-                    </div>
-                </div>
-                <input type='text' placeholder='Search' onChange={event => handleChange(event)}></input>
-            </form>
-        </div>
+        </nav>
     )
 }
 
 export default NavBar;
+
+
+
+
+
+
+// const NavBar = ({goHome, handleChange}) => {
+//     return(
+//         <div className='nav-bar-container'>
+//             <div className='title-img'>
+//                 <h1>Rancid Tamatillos</h1>
+//             </div>
+//             <form className='button-input-container'>
+//                 <button className='home' onClick={() => goHome()}>Home</button>
+//                 <div className='dropdown'>
+//                     <button className='categories'>Categories</button>
+//                     <div className='dropdown-content'>
+//                         <a href='#'>Action</a>
+//                         <a href='#'>Adventure</a>
+//                         <a href='#'>Horror</a>
+//                         <a href='#'>Comedy</a>
+//                     </div>
+//                 </div>
+//                 <input type='text' placeholder='Search' onChange={event => handleChange(event)}></input>
+//             </form>
+//         </div>
+//     )
+// }
