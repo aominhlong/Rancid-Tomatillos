e81b5a3defb6eebf7325fc22ab4ca500d5cf64cb
Kal <kal.aalrajhi@gmail.com>
2022-05-15 18:05:26 -0600

    Complete nav bar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,31 @@ import '../styles/NavBar.css'
 
 const NavBar = ({ goHome, handleChange }) => {
     return(
-        <nav className="main-navbar" aria-label="Main">
-            <div className="container flex" aria-description="Navigation container with site name and navigation options">
-                <h1 className="site-title">Rotten Tamatillos</h1>
-                <nav className="nav-selection">
-                    <ul>
-                        <li aria-setsize="3" aria-posinset="1"><button className="main-link" id="navHomeBtn">Home</button></li>
-                        <li aria-setsize="3" aria-posinset="2"><button className="main-link" id="navDashboardBtn">Dashboard</button></li>
-                        <li aria-setsize="3" aria-posinset="3"><button className="main-link" id="navBookBtn">Book</button></li>
-                    </ul>
-                </nav>
-            </div>
+        <nav aria-description='Navigation container with site name and navigation options'>
+            <ul className='nav-selection'>
+                <li className='site-title' aria-setsize='4' aria-posinset='1'>
+                    <h1>RANCID TOMATILLOS</h1>
+                </li>
+                <li aria-setsize='4' aria-posinset='2'>
+                    <button className='nav-btn home-btn' onClick={() => goHome()}>HOME</button>
+                </li>
+                <li aria-setsize='4' aria-posinset='3'>
+                    <div className='dropdown'>
+                        <button className='nav-btn categories-btn'>GENRE</button>
+                        <div className='dropdown-content'>
+                            <a href='#'>Action</a>
+                            <a href='#'>Adventure</a>
+                            <a href='#'>Horror</a>
+                            <a href='#'>Comedy</a>
+                        </div>
+                    </div>
+                </li>
+                <li aria-setsize='4' aria-posinset='4'>
+                    <form>
+                        <input type='text' placeholder='Search' onChange={event => handleChange(event)}></input>
+                    </form>
+                </li>
+            </ul>
         </nav>
     )
 }
@@ -33,15 +47,15 @@ export default NavBar;
 //             </div>
 //             <form className='button-input-container'>
 //                 <button className='home' onClick={() => goHome()}>Home</button>
-//                 <div className='dropdown'>
-//                     <button className='categories'>Categories</button>
-//                     <div className='dropdown-content'>
-//                         <a href='#'>Action</a>
-//                         <a href='#'>Adventure</a>
-//                         <a href='#'>Horror</a>
-//                         <a href='#'>Comedy</a>
-//                     </div>
-//                 </div>
+                // <div className='dropdown'>
+                //     <button className='categories'>Categories</button>
+                //     <div className='dropdown-content'>
+                //         <a href='#'>Action</a>
+                //         <a href='#'>Adventure</a>
+                //         <a href='#'>Horror</a>
+                //         <a href='#'>Comedy</a>
+                //     </div>
+                // </div>
 //                 <input type='text' placeholder='Search' onChange={event => handleChange(event)}></input>
 //             </form>
 //         </div>
