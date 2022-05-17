e6730ba2c782a8f24820e9963efeb6577937062d
Nicholas Ao <aominhlong@yahoo.com>
2022-05-16 19:28:35 -0600

    Add working video player and package of react-player to package.json

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
 import '../styles/MovieDetails.css'
 
 const MovieDetails = (props) => {
     const {id, title, poster_path, backdrop_path, release_date, 
             overview, average_rating, genres, budget, revenue, 
             runtime, tagline} = props.movieDetails.movie;
-            console.log('here', props.movieVideos.videos[0].key)
-    const {movieVideos} = props.movieVideos.videos[0].key
-            console.log('movie detail props', props)
+    const movieVideos = props.movieVideos.videos
+        console.log('movie videos: ', movieVideos)
     return(
         <>
             <header className="showcase">
@@ -33,15 +33,7 @@ const MovieDetails = (props) => {
                             <li>{`Budget: $${budget.toFixed(2)}`}</li>
                             <li>{`Revenue: $${revenue.toFixed(2)}`}</li>
                         </ul>
-                        <iframe 
-                            src={`https://www.youtube.com/embed/${movieVideos}`} 
-                            width="420" 
-                            height="315"
-                            frameBorder='0'
-                            allow='autoplay; encryped-media'
-                            allowFullScreen
-                            title={title} 
-                        />
+                        <ReactPlayer url={`https://www.youtube.com/watch?v=${movieVideos[0].key}`} />
                     </article>
                 </section>
             </header>
