4a508f1547b87067144b0d1597d8b9f1da4e1a60
Nicholas Ao <aominhlong@yahoo.com>
2022-05-22 16:00:02 -0600

    Add new styling to title

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,12 @@ const NavBar = ({ goHome, handleChange, searchBarValue }) => {
         <nav aria-description='Navigation container with site name and navigation options'>
             <ul className='nav-selection'>
                 <li className='site-title' aria-setsize='4' aria-posinset='1'>
-                    <h1>RANCID TOMATILLOS</h1>
+                    <h1 className='glow'>RA
+                        <span className='flicker-slow'>N</span>C
+                        <span className='flicker-fast'>I</span>D TO
+                        <span className='flicker-slow'>M</span>ATI
+                        <span className='flicker-fast'>L</span>LOS
+                    </h1>
                 </li>
                 <li aria-setsize='4' aria-posinset='2'>
                 <NavLink to='/'>
