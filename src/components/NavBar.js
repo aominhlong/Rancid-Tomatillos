404348df84ce4b928789529756a9e03779f61e93
Nicholas Ao <aominhlong@yahoo.com>
2022-05-21 18:03:07 -0600

    Add semicolons for readability in NavBar file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/NavBar.css'
+import '../styles/NavBar.css';
 
 const NavBar = ({ goHome, handleChange, searchBarValue }) => {
     return(
